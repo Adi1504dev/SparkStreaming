3ab511c9d6476f201e0080230c4f8320dd4e9b19
Adi1504dev <adityasingh.cs1504@gmail.com>
2021-11-01 13:49:55 +0530

    Kafka Stream--MultiQuery

--- src/main/scala/Code/a6_MultiQuery.scala
+++ src/main/scala/Code/a6_MultiQuery.scala
@@ -1,13 +1,13 @@
 package Code
 
-import Code.a4_SparkKafkaStream.logger
+import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, expr, from_json}
-import org.apache.spark.sql.streaming.Trigger
-
 import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
 
-/*
+object a6_MultiQuery extends Serializable {
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+  /*
    *
    * Start zookeeper and kafka
    * create topic-->kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic invoices
@@ -16,12 +16,15 @@ import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, LongType,
    * Send atleast one record and then run program
    *kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic notifications --from-beginning
    */
-object a5_SparkKafkaSink extends Serializable {
-  def main(args: Array[String]): Unit = {
+
+  def main(args:Array[String]): Unit =
+  {
+    //If input source is same and we have 2 operations(Write Query)
+    //like we created 2 applications above one to write in file sink one to write in kafka sink we can do it in same program.
 
     val spark = SparkSession.builder()
       .master("local[3]")
-      .appName("Kafka Stream Demo")
+      .appName("Kafka Sink Demo")
       .config("spark.streaming.stopGracefullyOnShutdown", "true")
       .getOrCreate()
 
@@ -56,34 +59,25 @@ object a5_SparkKafkaSink extends Serializable {
       )))),
     ))
 
-    val kafkaDF = spark
-      .readStream//while processing the stream you can not use show but we usually want to see data while processing
-      //So u can use read for while writing code and then later change it to readStream
+    val kafkaSourceDF = spark
+      .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "invoices")
       .option("startingOffsets", "earliest")
       .load()
 
-    //kafkaDF.printSchema()
-
-    val valueDF = kafkaDF.select(from_json(col("value").cast("string"), schema).alias("value"))
+    val valueDF = kafkaSourceDF.select(from_json(col("value").cast("string"), schema).alias("value"))
 
     val notificationDF = valueDF.select("value.InvoiceNumber", "value.CustomerCardNo", "value.TotalAmount")
       .withColumn("EarnedLoyaltyPoints", expr("TotalAmount * 0.2"))
-    //When we are writing to Kafka Sink Our DF should have 2 values key and value
-    //so here we have converted the invoice number to key and rest columns to value
-    val kafkaTargetDF = notificationDF.selectExpr("InvoiceNumber as key", "to_json(struct(CustomerCardNo,TotalAmount,EarnedLoyaltyPoints)) as value")
-    //kafkaTargetDF.show()//could be used only with spark.read
 
-    //Second way of executing above command for converting in key value DF
-    /*
     val kafkaTargetDF = notificationDF.selectExpr("InvoiceNumber as key",
       """to_json(named_struct('CustomerCardNo', CustomerCardNo,
         |'TotalAmount', TotalAmount,
         |'EarnedLoyaltyPoints', TotalAmount * 0.2
         |)) as value""".stripMargin)
-*/
+
     val notificationWriterQuery = kafkaTargetDF
       .writeStream
       .queryName("Notification Writer")
@@ -91,10 +85,8 @@ object a5_SparkKafkaSink extends Serializable {
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("topic", "notifications")
       .outputMode("append")
-      .option("checkpointLocation", "chk-point-dir")
+      .option("checkpointLocation", "chk-point-dir/notify")//each query should have unique check point location
       .start()
-
-    logger.info("Listening and writing to Kafka")
 
     val explodeDF = valueDF.selectExpr("value.InvoiceNumber", "value.CreatedTime", "value.StoreID",
       "value.PosID", "value.CustomerType", "value.PaymentMethod", "value.DeliveryType", "value.DeliveryAddress.City",
@@ -113,12 +105,14 @@ object a5_SparkKafkaSink extends Serializable {
       .queryName("Flattened Invoice Writer")
       .outputMode("append")
       .option("path", "output")
-      .option("checkpointLocation", "chk-point-dir/flatten")
+      .option("checkpointLocation", "chk-point-dir/flatten")//each query should have unique check point location
       .start()
 
+    //Both of the queries will run in parallel
+    //by below command and and application will stop only in one condition if any query fails or program is stopped for maintenance
+
     logger.info("Waiting for Queries")
-    spark.streams.awaitAnyTermination()
-
-
+    spark.streams.awaitAnyTermination()//If sny onr of the query is terminated the program will stop.
   }
+
 }
